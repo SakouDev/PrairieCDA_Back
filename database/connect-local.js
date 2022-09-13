7881cdf8e54afea990548811e1ff6f96c1967ee8
Sakou <86302989+SakouDev@users.noreply.github.com>
2022-09-13 18:14:47 +0200

    Menfou

--- database/connect-local.js
+++ database/connect-local.js
@@ -4,8 +4,8 @@ const db = new DB({
     host: "localhost",
     user: "postgres",
     port: 5432,
-    password: "12344",
-    database: "prairiewafmiaou"
+    password: "b87ce6a6a1e",
+    database: "postgres"
 })
 
 module.exports = db
